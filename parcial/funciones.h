28a66883bed9cc98c9fb98303ea3d14c39dbc8c2
KevinMartinez1999 <kevin.martinez1@udea.edu.co>
2020-05-09 00:00:50 -0500

    Modificaciones a la interfaz principal y funciones nuevas en la interfaz de administrador y usuario

--- parcial/funciones.h
+++ parcial/funciones.h
@@ -34,6 +34,7 @@ void interfaz();
 void interfaz_de_admin();
 void ingresar_pelicula();
 void asientos_disponibles();
+void generar_reporte();
 
 void interfaz_de_usuario();
 
@@ -44,6 +45,7 @@ void interfaz_de_usuario();
  */
 
 void interfaz(){
+    limpiar_pantalla();
     int opcion=0;
     do{
         limpiar_pantalla();
@@ -67,38 +69,91 @@ void interfaz(){
 void interfaz_de_admin(){
     limpiar_pantalla();
     int opcion=0;
-    cout<<"1. Ingresar pelicula\nElija una opcion: "; cin>>opcion;
+    cout<<"1. Ingresar pelicula\n";
+    cout<<"2. Consultar asientos disponibles\n";
+    cout<<"3. Generar reporte\n";
+    cout<<"Elija una opcion: "; cin>>opcion;
     switch(opcion) {
     case 1:
         ingresar_pelicula();
+        break;
+    case 2:
+        asientos_disponibles();
+        break;
+    case 3:
+        generar_reporte();
         break;
     }
 }
 
 //ingresa los daros de la pelicula y van a la base de datos
 void ingresar_pelicula(){
-    string nombre, genero, duracion, sala, hora, asientos, clas;
-    cout<<"Nombre   : "; cin>>nombre;
-    cout<<"Genero   : "; cin>>genero;
-    cout<<"Duracion : "; cin>>duracion;
-    cout<<"Sala     : "; cin>>sala;
-    cout<<"Hora     : "; cin>>hora;
-    cout<<"Asientos : "; cin>>asientos;
-    cout<<"Clasif.  : "; cin>>clas;
-    cout<<nombre<<endl;
-    cin.ignore().get();
-    ofstream file("peliculas.txt");
+    cin.ignore();
+    char nombre[20], genero[20], duracion[20], sala[20], hora[20], asientos[20], clas[20];
+    cout<<"Nombre   : "; cin.getline(nombre, 20, '\n');
+    cout<<"Genero   : "; cin.getline(genero, 20, '\n');
+    cout<<"Duracion : "; cin.getline(duracion, 20, '\n');
+    cout<<"Sala     : "; cin.getline(sala, 20, '\n');
+    cout<<"Hora     : "; cin.getline(hora, 20, '\n');
+    cout<<"Asientos : "; cin.getline(asientos, 20, '\n');
+    cout<<"Clasfic. : "; cin.getline(clas, 20, '\n');
+    ofstream file("cartelera.txt", ios::binary);
     file<<nombre<<" "<<genero<<" "<<duracion<<" "<<sala<<" ";
-    file<<hora<<" "<<asientos<<" "<<clas<<"\n";
+    file<<hora<<" "<<asientos<<" "<<clas<<'\n';
     file.close();
 }
 
 void asientos_disponibles(){
+    cin.ignore();
+    char nombre[20], genero[20], duracion[20], sala[20], hora[20], asientos[20], clas[20];
+    char pelicula[20];
+    cout<<"Nombre   : "; cin.getline(pelicula, 20, '\n');
+    ifstream file("cartelera.txt");
+    if(file.is_open()){
+        file>>nombre;
+        while(!file.eof()){
+            file>>genero;
+            file>>duracion;
+            file>>sala;
+            file>>hora;
+            file>>asientos;
+            file>>clas;
+            if(nombre==string(pelicula)){
+                cout<<"Hay "<<asientos<<" disponibles"<<endl;
+                cin.ignore();
+            }
+            file>>nombre;
+        }
+    }
+    file.close();
+}
+
+void generar_reporte(){
 
 }
 
 void interfaz_de_usuario(){
-
+    cout<<"\n\n";
+    printf("%10s  ", "nombre"); printf("%10s  ", "Genero"); printf("%8s  ", "Duracion"); printf("%4s  ", "Sala");
+    printf("%4s  ", "Hora"); printf("%8s  ", "Asientos"); printf("%13s  ", "Clasificacion"); cout<<"\n\n";
+    char nombre[20], genero[20], duracion[20], sala[20], hora[20], asientos[20], clas[20];
+    ifstream file("cartelera.txt");
+    if(file.is_open()){
+        file>>nombre;
+        while(!file.eof()){
+            file>>genero;
+            file>>duracion;
+            file>>sala;
+            file>>hora;
+            file>>asientos;
+            file>>clas;
+            printf("%10s  ", nombre); printf("%10s  ", genero); printf("%8s  ", duracion); printf("%4s  ", sala);
+            printf("%4s  ", hora); printf("%8s  ", asientos); printf("%13s  ", clas); printf("\n");
+            file>>nombre;
+        }
+    }
+    file.close();
+    cin.ignore().get();
 }
 
 #endif // FUNCIONES_H
