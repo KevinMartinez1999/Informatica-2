b2a603042e9af7422eda90bd173df4ed7bf7890c
KevinMartinez1999 <kevin.martinez1@udea.edu.co>
2020-05-13 16:35:38 -0500

    Interfaz de usuario lista; la generacion del reporte de ventas tambien lista

--- parcial/funciones.h
+++ parcial/funciones.h
@@ -2,11 +2,9 @@
 #define FUNCIONES_H
 
 #include <iostream>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
 #include <fstream>
 #include <string.h>
+#include <map>
 
 using namespace std;
 
@@ -32,7 +30,9 @@ void user();
 int mostrar_funciones();
 string seleccionar_funcion(int a);
 void seleccionar_asiento(string);
-void imprimir_sala(int);
+void imprimir_sala(string, map<char, int> &);
+void comprar(string, char, char, char, map<char, int> &);
+void reporte(map<char, int> &, char, char);
 
 void menu(){
     int a=0;
@@ -41,7 +41,7 @@ void menu(){
         cout<<"1. Administrador\n";
         cout<<"2. Usuario\n";
         cout<<"3. Salir\n";
-        cout<<"Selecciones una: "; cin>>a;
+        cout<<"\nSelecciones una: "; cin>>a;
         switch (a) {
         case 1:
             admin();
@@ -56,8 +56,29 @@ void menu(){
 }
 
 void admin(){
-
+    int a=0;
+    do{
+        limpiar_pantalla();
+        cout<<"1. Agregar pelicula\n";
+        cout<<"2. Mostrar asientos disp.\n";
+        cout<<"3. Generar registro\n";
+        cout<<"\nSelecciones una: "; cin>>a;
+        switch (a) {
+        case 1:
+            agregar_pelicula();
+            break;
+        case 2:
+            ofertar_asientos();
+            break;
+        case 3:
+            generar_reporte();
+            break;
+        default:
+            break;
+        }
+    }while(a!=4);
 }
+
 void agregar_pelicula(){
 
 }
@@ -65,7 +86,20 @@ void ofertar_asientos(){
 
 }
 void generar_reporte(){
-
+    limpiar_pantalla();
+    cout<<"<-----REPORTE DE ENTRADAS DE CINE----->\n\n";
+    ifstream file("../parcial/reporte.txt");
+    string s;
+    if(file.is_open()){
+        file>>s;
+        while(!file.eof()){
+            cout<<s<<endl;
+            file>>s;
+        }
+    }
+    else
+        cout<<"No hay un reporte aun..."<<endl;
+    cin.ignore().get();
 }
 
 void user(){
@@ -76,7 +110,7 @@ void user(){
 }
 
 int mostrar_funciones(){
-    int cont, op=0;
+    int cont, op, num=0;
     cout<<endl;
     cout<<"<----------------------------"
           "----------FUNCIONES----------"
@@ -108,10 +142,15 @@ int mostrar_funciones(){
             }
             cont++;
         }
+        num++;
         cout<<endl;
     }
     file.close();
-    cout<<"Seleccionar: "; cin>>op;
+    cout<<"Seleccionar (ID): "; cin>>op;
+    while(op>num){
+        cout<<"Esta funcion no existe.\n";
+        cout<<"Seleccionar (ID): "; cin>>op;
+    }
     return op;
 }
 
@@ -146,51 +185,148 @@ string seleccionar_funcion(int a){
 
 void seleccionar_asiento(string sala){
     limpiar_pantalla();
-    printf("%4s  ", "Tipo"); printf("%13s  ", "Clasificacion"); printf("%6s\n\n", "Costo");
-    printf("%4s  ", "1"); printf("%13s  ", "General 2D"); printf("%6s\n", "7900$");
-    printf("%4s  ", "2"); printf("%13s  ", "General 3D"); printf("%6s\n", "10800$");
-    printf("%4s  ", "3"); printf("%13s  ", "VibroSound 2D"); printf("%6s\n", "9900$");
-    printf("%4s  ", "4"); printf("%13s  ", "VibroSound 3D"); printf("%6s\n", "11900$");
+    map<char, int> costos;
+    costos['1']=7900; costos['2']=10800; costos['3']=9900; costos['4']=11900;
+    printf("%4s  ", "Tipo"); printf("%13s  ", "Clasificacion"); printf("%6s  ", "Costo"); printf("%7s\n\n", "Simbolo");
+    printf("%4s  ", "1"); printf("%13s  ", "General 2D"); printf("%6u  ", costos['1']); printf("%4c\n", '-');
+    printf("%4s  ", "2"); printf("%13s  ", "General 3D"); printf("%6u  ", costos['2']); printf("%4c\n", '-');
+    printf("%4s  ", "3"); printf("%13s  ", "VibroSound 2D"); printf("%6u  ", costos['3']); printf("%4c\n", '=');
+    printf("%4s  ", "4"); printf("%13s  ", "VibroSound 3D"); printf("%6u  ", costos['4']); printf("%4c\n", '=');
     cout<<"<----------------------------"
           "----------    /    ----------"
           "---------------------------->\n\n";
     int op=stoi(sala)-3;
     switch(op){
     case 1:
-        imprimir_sala(50);
+        imprimir_sala("../parcial/salas/sala1.txt", costos);
         break;
     case 2:
     case 3:
     case 4:
     case 7:
-        imprimir_sala(140);
+        imprimir_sala("../parcial/salas/sala2347.txt", costos);
         break;
     case 5:
     case 6:
-        imprimir_sala(100);
+        imprimir_sala("../parcial/salas/sala56.txt", costos);
         break;
     }
-
-    char fila, col;
-    cout<<"\nSelecciona fila: "; cin>>fila;
-    cout<<"Selecciona columna: "; cin>>col;
-
-    cin.ignore().get();
 }
 
-void imprimir_sala(int asientos){
-    int filas=asientos/10, col=10;
+void imprimir_sala(string sala, map<char, int> &costos){
+    char letra=65;
     cout<<"  ";
-    for(int i=1; i<=col; i++)
+    for(int i=1; i<=10; i++)
         printf("%2u ", i);
     cout<<endl;
-    for(int i=1; i<=filas; i++){
-        printf("%1c ", char(i+64));
-        for(int j=1; j<=col; j++){
-            printf("%2c ", '-');
+    ifstream file(sala);
+    char contenido;
+    int cont=0;
+    if(file.is_open()){
+        printf("%1c ", char(letra++));
+        file>>contenido;
+        while(!file.eof()){
+            if(cont==10){
+                cout<<endl;
+                printf("%1c ", char(letra++));
+                cont=0;
+            }
+            printf("%2c ", contenido);
+            file>>contenido;
+            cont++;
         }
-        cout<<endl;
     }
+    else
+        cout<<"No abierto..."<<endl;
+
+    char fila, col, gafas;
+    cout<<"\n\nSelecciona fila: "; cin>>fila;
+    fila=toupper(fila);
+    while(int(fila)<65 and int(fila)>90){
+        cout<<"Asiento no disponible...\n";
+        cout<<"\n\nSelecciona fila: "; cin>>fila;
+        fila=toupper(fila);
+    }
+    cout<<"Selecciona columna: "; cin>>col;
+    while(int(col<49) and int(col)>57){
+        cout<<"Asiento no disponible...\n";
+        cout<<"\n\nSelecciona columna: "; cin>>col;
+    }
+    cout<<"Gafas 3D (y/n):"; cin>>gafas;
+    gafas=tolower(gafas);
+    while(gafas!='y' and gafas!='n'){
+        cout<<"Gafas 3D (y/n):"; cin>>gafas;
+        gafas=tolower(gafas);
+    }
+    comprar(sala, fila, col, gafas, costos);
+}
+
+void comprar(string sala, char fila, char col, char gafas, map<char, int> &costos){
+    map<char, int> mapa1;
+    map<char, int> mapa2;
+    int pos;
+    int val_fila=0, val_col=1;
+    for(int i=65; i<=90; i++){
+        mapa1[char(i)]=val_fila;
+        val_fila+=10;
+    }
+    for(int i=1; i<=10; i++){
+        mapa2[char(i+48)]=val_col;
+        val_col++;
+    }
+    pos=mapa1[fila]+mapa2[col];
+    int cont=0, saltos=0;
+    ofstream salida("../parcial/salas/nuevo.txt");
+    ifstream file(sala);
+    char contenido;
+    if(file.is_open()){
+        file>>contenido;
+        cont++;
+        saltos++;
+        while(!file.eof()){
+            if(cont==pos){
+                salida<<'x';
+                if(saltos==10){
+                    salida<<'\n';
+                    saltos=0;
+                }
+                reporte(costos, contenido, gafas);
+            }
+            else{
+                salida<<contenido;
+                if(saltos==10){
+                    salida<<'\n';
+                    saltos=0;
+                }
+            }
+            cont++;
+            saltos++;
+            file>>contenido;
+        }
+    }
+    file.close();
+    salida.close();
+    remove(sala.c_str());
+    rename("../parcial/salas/nuevo.txt", sala.c_str());
+}
+
+void reporte(map<char, int> &costos, char contenido, char gafas){
+    int valor=0;
+    if(contenido=='='){
+        if(gafas=='y')
+            valor=costos['4'];
+        else
+            valor=costos['3'];
+    }
+    else if(contenido=='-'){
+        if(gafas=='y')
+            valor=costos['2'];
+        else
+            valor=costos['1'];
+    }
+    ofstream file("../parcial/reporte.txt");
+    file<<valor<<'\n';
+    file.close();
 }
 
 #endif // FUNCIONES_H
